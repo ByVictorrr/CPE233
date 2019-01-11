47046f7e17641a74a54b96bb38e79373eb01f387
Victor Delaplaine <vdelaplaines@aol.com>
2019-01-11 22:05:02 +0000

    autoadd

--- RAT/RAT1/RAT1_Lab1/ProgRom_tb.sv
+++ RAT/RAT1/RAT1_Lab1/ProgRom_tb.sv
@@ -22,28 +22,41 @@
 
 module ProgRom_tb();
 
- logic CLK;
- logic [9:0] ADDR;
- logic [17:0] IR;
+ logic PROG_CLK;
+ logic [9:0] PROG_ADDR;
+ logic [17:0] PROG_IR;
  
   ProgRom UUT (
-      .PROG_CLK(CLK),
-      .PROG_ADDR(ADDR),
-      .PROG_IR(IR)
+      .PROG_CLK(PROG_CLK),
+      .PROG_ADDR(PROG_ADDR),
+      .PROG_IR(PROG_IR)
       );
   
- initial 
- begin
- CLK = 0; 
- ADDR = 'h000;
- IR = 'h00000;
- #10;
- 
- CLK = 1;   
- ADDR = 'h000;
- #10;
- 
- 
- end
- 
+ initial begin
+  PROG_CLK = 0;
+  forever #5 PROG_CLK = ~PROG_CLK; //every 5ns clock changes period of 10ns
+  end 
+  
+  initial begin
+  // Initialize Inputs
+  PROG_ADDR = 'h000;
+  PROG_IR = 'h00000;
+  // Wait 100 ns for global reset to finish
+   #10;
+   PROG_ADDR = 'h001;
+   #10
+   PROG_ADDR = 'h002;
+   #10
+   PROG_ADDR = 'h003;
+   #10
+   PROG_ADDR = 'h004;
+   #10
+   PROG_ADDR = 'h005;
+   #10
+   PROG_ADDR = 'h006;
+   #10
+   PROG_ADDR = 'h007;
+   #10
+
+
 endmodule
