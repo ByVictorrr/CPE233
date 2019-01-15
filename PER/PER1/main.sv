618dca29f4bdbdef35b1a516ee988aa3244ea2c5
byvictorrr <vdelaplaines@aol.com>
2019-01-15 21:57:17 +0000

    added

---
+++ PER/PER1/main.sv
@@ -0,0 +1,13 @@
+module tones(input CLK, input [7:0]SW, output TONES)
+
+//f_CLK_input = 100 MHz
+//
+// SW = {}
+//
+// frequency output should change if SWithes change
+
+//USE MUX that selects for differnt values of SW's to choose the frequency wanted.
+// Coming out of p-mod of basys3 board will go into breadboard
+
+endmodule
+
