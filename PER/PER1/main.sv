301d51ade5a8221f89b66920407fed79be3698da
Victor Delaplaine <vdelaplaines@aol.com>
2019-01-16 04:05:01 +0000

    autoadd

--- PER/PER1/main.sv
+++ PER/PER1/main.sv
@@ -1,4 +1,10 @@
-module tones(input CLK, input [7:0]SW, output TONES)
+`include "./ClockDivider.sv"
+`include "/home/victor/CPE233/Modules/mux_4t1_nb.v"
+`include "./SW_DCDR_SCLK.sv"
+
+
+
+module tones(input CLK, input [7:0]SW, output TONES);
 
 //f_CLK_input = 100 MHz
 //
@@ -8,6 +14,20 @@ module tones(input CLK, input [7:0]SW, output TONES)
 
 //USE MUX that selects for differnt values of SW's to choose the frequency wanted.
 // Coming out of p-mod of basys3 board will go into breadboard
+
+wire SCLK;
+
+SW_DCDR_SCLK decoder(); // maps switches to output clk
+
+ClockDivider CLK_DIV(
+    .clk(CLK), 
+    .maxcount(),
+    .sclk(SCLK)  
+    );
+
+
+
+
 
 endmodule
 
