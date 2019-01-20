48233541928118243f90dbdb956068a0f6c91e64
Victor Delaplaine <vdelaplaines@aol.com>
2019-01-20 09:43:28 +0000

    aded

--- SW/SW2/part2.asm
+++ SW/SW2/part2.asm
@@ -1,15 +1,25 @@
 ;--------------------------------------------------------------------
-; Software Assignment 2.1
+; Software Assignment 2.2
 ; Author: Victor Delaplaine
 ; Date: 1/14/19
-; Description : Reads in an 80bit unsigned value (X) from port 0x30. 
-;		If (X % 4 =0){ 
-		;x is even => 
-		
-		;} 128, then the value X/4.
-;		Else, th;en the value X*2;
+; Description : Reads in an 8 bit unsigned value (X) from port 0x30. 
+;		If (X % 4 =0 (if x is div by 4)){ 
+;		    X=0-X,
+;		    X= X xor 0xFF
+;		    X=>OUT_PORT
+;		}
+;		else{ (if x isnt div by 4)
+;			if(X % 2 = 1 (if x is odd)){
+;			X=X+17
+;			X=X/2
+;			X=>OUT_PROT	
+;			}
+;			else{ X is even
+;			X=X-1			
+;			X=>OUT_PROT	
+;			}	
 ;		This result should be ourput on port 0x42
-;
+;		}
 ; Register uses:
 ; R0 - holds the result (res)
 ; R1 - holds the temp (temp) for masking
@@ -27,6 +37,7 @@ main:		IN R0, IN_PORT
 		AND R1, 0x03 ; masking (temp = temp &n 0x03 picking out 1's)
 		CMP R1, 0x00 ;( if R1 = 0000000..0 then div by 4)
 		BREQ div4 ; if(div by four) => jump to div4
+		
 		MOV R1, R0 ; R1=R0
 		AND R1, 0x01 ; to check if x is even or odd
 	
@@ -34,12 +45,12 @@ main:		IN R0, IN_PORT
 		CLC
 		CMP R1, 0x01;
 		BREQ odd ; (z = 1)
-		ADD R0 = R0 - 0x01	 ; if even
+		XOR R1, 0xFF
 		BRNE output ;
 
 odd: 		ADD R0, 0x12 ; (X = X + 17)
 		CLC ; (clear carry flag)
-		RSR R0; 
+		LSR R0; 
 		BRNE output
 
 
