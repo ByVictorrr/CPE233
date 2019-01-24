8be8736ef190b6a75bc9ec67bc44fdb4d68fd6a3
Victor Delaplaine <vdelaplainess@gmail.com>
2019-01-24 14:32:45 -0800

    Update SCRATCH_RAM_tb.sv

--- RAT/RAT3/SCRATCH_RAM_tb.sv
+++ RAT/RAT3/SCRATCH_RAM_tb.sv
@@ -1,13 +1,13 @@
-module SCTRACH_RAM_tb;
+module SCRATCH_RAM_tb;
 
-	logic [7:0] DATA_IN;
-	logic [4:0] SCR_ADDR;
+	logic [9:0] DATA_IN;
+	logic [7:0] SCR_ADDR;
 	logic SCR_WE;
 	logic CLK;
-	logic [7:0] DATA_OUT;
+	logic [9:0] DATA_OUT;
 		
 
-REG_FILE UUT(.*);
+SCRATCH_RAM UUT(.*);
 
 
 initial begin
@@ -16,49 +16,55 @@ initial begin
   end 
 
 initial begin
+//test 1 - check if each reg stores a zero
+//==============================================
+//CLK = 0
 
-#5 //CLK = 0
-
-	SCR_ADDR = 100; //arbitary
+	SCR_ADDR = 0'h64; //arbitary
 	SCR_WE = 0; // aysnc read
+	SCR_WE=0;
+	DATA_IN = 0;
+
+//==============================================
 
 
-#5 
-
-// CLK=1
 
 //test 2 - check if the synch write works
+//==============================================
 #10	//CLK = 1
 
 	SCR_ADDR = 3;
 	SCR_ADDR = 5;
-	DATA_IN = 255;
+	DATA_IN = 0'hFF;
 	SCR_WE = 1; // test write control signal
 	
 #5 //CLK = 0
 
 //check if SCR_ADDR was written to
-	
+	SCR_ADDR = 3;
 	SCR_WE = 0;
+//=================================================
 
 
 //test 3 - write to all the memory address
-
+//===============================================
 #5 //CLK = 1
 
-
-	for(int i = 0; i<256; i++) begin
+	for(int i = 0; i<1023; i++) begin
 		DATA_IN = i;
 		SCR_WE = 1;
 		SCR_ADDR = i;
-
+		#10; //write to next spot i
 	end
-
-
-
+	
+// make sure each reg[i] = i	
+#5 //CLK = 0
+	for(int i = 0; i<1023; i++) begin
+        SCR_WE = 0;
+        SCR_ADDR = i;
+        #5; //read at times of 5ns
+        end
+//=================================================    
 end
-
-
-
 
 endmodule
