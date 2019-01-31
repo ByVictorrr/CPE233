dbb1d2f142fa61f5605a9845362af7e93c2fe4d7
Victor Delaplaine <vdelaplaines@aol.com>
2019-01-30 22:05:01 -0800

    autoadd

--- PER/PER2/main_tb.sv
+++ PER/PER2/main_tb.sv
@@ -9,26 +9,16 @@ main UUT(.*);
 
 initial begin
   CLK = 1;
-  forever #5 CLK = ~CLK; //every 5ns clock changes period of 10ns
+  forever #1 CLK = ~CLK; //every 5ns clock changes period of 10ns
   end 
 
 initial begin
 //test 1 - check if each reg stores a zero
 //==============================================
 //CLK = 0
-	SW = 1;
-
-//test 2 - check if
-//==============================================
-#10	//CLK = 1
-
-	for(int i = 1; i< 256; i++)
-	begin
-		SW = i;
-		#10;
-	end
+	SW = 10;
 	
-
+#200;
 
 //=================================================    
 end
