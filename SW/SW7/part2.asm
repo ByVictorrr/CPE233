37f7d2fca2b00cb5f747903873abc4f01c10d3a6
Victor Delaplaine <vdelaplaines@aol.com>
2019-02-21 23:17:53 -0800

    autoadd

--- SW/SW7/part2.asm
+++ SW/SW7/part2.asm
@@ -1,70 +1,57 @@
 ;--------------------------------------------------------------------
-; Software Assignment 7.1
+; Software Assignment 7.2
 ; Author: Victor Delaplaine
 ; Date: 2019-02-20
-; Description : Create a subroutin that will divide a value by 10, then use the soubtouine to
-;		convert an unsigned 8-bit value into a 3 digit binary coded decimal.
-;		The main program should read in the value X from port_id 0x9A.
-;		The value will tbe converted into a BC of 3 values 100's 10s and 1's places,
-;		Finally 1s is outputed on port 0x43, 10th on port 0x42, and 100s on port 41.
+; Description : Create a subroutine that will help you multiply to values inputed on port 0x9A,
+;		These values are R3, and R4; 
+;		Any carry that is generated in this subroutine (ADD16) will be added to a seperate
+;		register(R2) as part of the 16-bit result, the lower 8 bits is stored in R0.
+;		Output the upper 8bits(R2) to port 0x42 and lower(R0) to 0x41
 ;
 ; Register uses:
-; R0 - X main value 
-; R1 - Ones 
-; R2 - Tens
-; R3 - HUNDs
-; R4 - R
-; R5  -res
-; R6 - for masking temp
+; R0 - used as a parameter final value is lower 8 bits of the result
+; R1 - used as a parameter
+; R2 - upper 8 bits of the result
+; R3 - input value 
+; R4 - input value
 ;--------------------------------------------------------------------
 
-.EQU OUT_PORT_ONES =  0x43
-.EQU OUT_PORT_TENS =  0x42
-.EQU OUT_PORT_HUNDS =  0x41
+.EQU OUT_PORT_UPPER =  0x42
+.EQU OUT_PORT_LOWER = 0x41
 .EQU IN_PORT = 0x9A
-.EQU COUNT_DIV = 5 
 .CSEG
 .ORG 0x01
 
+
 main:
-		IN R0, IN_PORT
-		MOV R4, COUNT_DIV ; for loop 
-		CALL subroutine
-end:		BRN end
+		IN R3, IN_PORT
+		IN R4, IN_PORT
+		MOV R0, R3
+		MOV R1, R4
 
-
-
-subroutine:	MOV R4, R0
-		CMP R0, 0 
-		;if z== 1 R0 is zero
+multiply:	CMP R1, 0
 		BREQ output
-compare:	CMP R4, 10
-		;c== 1 if R is less than 10
-		BRCS outputs 
-		;else loop
-		SUB R4, 10;
-		ADD R5, 1;
-		BRN compare 
+		CALL ADD16
+		SUB R1, 1
+		BRN multiply
 
+output:		OUT R1, OUT_PORT_LOWER
+		OUT R2, OUT_PORT_UPPER
 
+;------------------------------------------------------------------------------------
+; ADD16 subroutine
+; Adds 2 - 8 bit numbers and stores them into a 16 bit result {R2, R0}
+; Parameters : R0 - value of an input
+;	       R1 - value of an input
+;	
+; Return : R0 - lower 8 bits of result
+;	   R2 - upper 8 bits of result
+; Tweaked Parmeter : R0
 
-outputs:	MOV R6, R5;
-		AND R6, 7; ;try to mask rightmost bits
-		MOV R1, R6 
- 
-		MOV R6, R5;
-		AND R6, 56 ; ;try to mask rightmost bits
-		MOV R2, R6 
- 
+ADD16:
+	ADD R0, R1;
+	ADD R2, 0 ;
+	RET 
+;------------------------------------------------------------------------------------
 
-		MOV R6, R5;
-		AND R6, 192; ;try to mask rightmost bits
-		MOV R3, R6 
- 
-
-
-		OUT R1, OUT_PORT_ONES 
-		OUT R2, OUT_PORT_TENS
-		OUT R3, OUT_PORT_HUNDS
-		RET
 
