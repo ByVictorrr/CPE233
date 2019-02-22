85d482550f1be7c4f6eed22c7820e08c1a3f2ea6
Victor Delaplaine <vdelaplaines@aol.com>
2019-02-22 00:25:10 -0800

    autoadd

--- SW/SW7/part2.asm
+++ SW/SW7/part2.asm
@@ -9,11 +9,14 @@
 ;		Output the upper 8bits(R2) to port 0x42 and lower(R0) to 0x41
 ;
 ; Register uses:
-; R0 - used as a parameter final value is lower 8 bits of the result
-; R1 - used as a parameter
-; R2 - upper 8 bits of the result
-; R3 - input value 
-; R4 - input value
+; R0 - parameter of MULT16 subroutine
+; R1 - parameter of MULT16 subroutine, also return of lower 8 bits
+; R2 - return of ADD16 subroutine, upper 8 bits
+; R3 - parameter of MULT16 subroutine 
+; R4 - parameter of MULT16 subroutine, also return of lower 8 bits
+; R5 - return of MULT16 subroutine, upper 8 bits
+; R6 - input reg
+; R7 - input reg
 ;--------------------------------------------------------------------
 
 .EQU OUT_PORT_UPPER =  0x42
@@ -24,34 +27,58 @@
 
 
 main:
-		IN R3, IN_PORT
-		IN R4, IN_PORT
-		MOV R0, R3
-		MOV R1, R4
+		IN R6, IN_PORT
+		IN R7, IN_PORT
+		MOV R3, R6
+		MOV R4, R7
+		CALL MULT16
+		
+	
+output:	OUT R4, OUT_PORT_LOWER
+		OUT R5, OUT_PORT_UPPER
 
-multiply:	CMP R1, 0
-		BREQ output
-		CALL ADD16
-		SUB R1, 1
-		BRN multiply
-
-output:		OUT R0, OUT_PORT_LOWER
-		OUT R2, OUT_PORT_UPPER
-
+end: BRN end ; branch to it self
 ;------------------------------------------------------------------------------------
 ; ADD16 subroutine
 ; Adds 2 - 8 bit numbers and stores them into a 16 bit result {R2, R0}
-; Parameters : R0 - value of an input
-;	       R1 - value of an input
+; Parameters : R0 a input
+;			 : R1 an input
+;	      
 ;	
-; Return : R0 - lower 8 bits of result
-;	   R2 - upper 8 bits of result
-; Tweaked Parmeter : R0
+; Return : R1 - lower 8 bits of result
+;	   	   R2 - upper 8 bits of result 
+; Tweaked Parmeter : R2
 
 ADD16:
-	ADD R0, R1;
+	CLC
+	ADD R1, R0;
 	ADDC R2, 0 ;
 	RET 
 ;------------------------------------------------------------------------------------
 
+;------------------------------------------------------------------------------------
+; MULT16 subroutine
+; Multiplies two 8 bit numbers and stores them into a 16 bit result {R5, R2}
+; Parameters : R3 - value of an input
+;	       	   R4 - value of an input
+;	
+; Return : R4 - lower 8 bits of result
+;	       R5 - upper 8 bits of result
+; Tweaked Parmeter : R4
+
+MULT16: MOV R0, R4
+
+MULT16_loop:
+		CMP R3, 0
+		BREQ ret_MULT16
+		CALL ADD16
+		SUB R3, 1
+		BRN MULT16_loop
+		
+ret_MULT16: 
+			MOV R4, R1 ; setting R0 to lower 8 bit return value
+			MOV R5, R2 ; setting R5 to higher 8 bit for return value
+			
+			RET	
+;------------------------------------------------------------------------------------
 
