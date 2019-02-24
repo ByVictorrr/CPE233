21999263c5b3c55ff206ab7162bbd59e209ba66f
Victor Delaplaine <vdelaplaines@aol.com>
2019-02-24 03:05:02 -0800

    autoadd

--- RAT/RAT7/RAT7/RAT7.runs/impl_1/rundef.js
+++ RAT/RAT7/RAT7/RAT7.runs/impl_1/rundef.js
@@ -27,7 +27,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log RAT_WRAPPER.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source RAT_WRAPPER.tcl -notrace" );
 
